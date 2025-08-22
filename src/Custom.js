699c39c8baf79e05173ca38fee2727014abfd685
Zixus <mahrez.cherif1@gmail.com>
2025-08-22 15:49:46 +0100

    working with the custom selection systeme and t-shirt personalisation

--- src/Custom.js
+++ src/Custom.js
@@ -1,29 +1,64 @@
 const nodeItems = document.querySelectorAll(".item-card");
-const shirt = document.getElementById("shirt");
-const cas = document.getElementById("casquette");
-const plate = document.getElementById("assiette");
-const mug = document.getElementById("mug");
+const nodeGender = document.querySelectorAll(".gender-card");
+const nodeSizebtn = document.querySelectorAll(".sizeBtn");
+const nodeColor = document.querySelectorAll(".shirt-color");
 
-function isSelected(arg) {
-  if (arg.tagName != "DIV") return;
-  arg.addEventListener("click", () => {
-    arg.className += " selected";
-    console.log(arg);
-  });
-}
-
-function toFocus(arg) {
-  if (arg.tagName != "DIV") return;
-  arg.addEventListener("click", () => {
-    arg.focus();
-    console.log(arg);
-  });
-}
-
+//iteme selections
 nodeItems.forEach((el) => {
-  el.addEventListener("click", (e) => {
-    const choice = document.getElementById(`${e.currentTarget.id}`);
-    isSelected(choice);
-    toFocus(choice);
+  el.addEventListener("click", () => {
+    nodeItems.forEach((item) => {
+      item.classList.remove("selected");
+      item.classList.add("notSelected");
+    });
+
+    el.classList.add("selected");
+    el.classList.remove("notSelected");
+
+    el.focus();
+  });
+});
+//gender selection
+
+nodeGender.forEach((el) => {
+  el.addEventListener("click", () => {
+    nodeGender.forEach((gender) => {
+      gender.classList.remove("selected");
+      gender.classList.add("notSelected");
+    });
+
+    el.classList.add("selected");
+    el.classList.remove("notSelected");
+
+    el.focus();
+  });
+});
+//size Selections
+nodeSizebtn.forEach((el) => {
+  el.addEventListener("click", () => {
+    nodeSizebtn.forEach((BTN) => {
+      BTN.classList.remove("selected");
+      BTN.classList.add("notSelected");
+    });
+
+    el.classList.add("selected");
+    el.classList.remove("notSelected");
+
+    el.focus();
+  });
+});
+//Color Selections
+nodeColor.forEach((el) => {
+  bgColor = el.id;
+  el.style.backgroundColor = bgColor;
+  el.addEventListener("click", () => {
+    nodeColor.forEach((BTN) => {
+      BTN.classList.remove("selected");
+      BTN.classList.add("notSelected");
+    });
+
+    el.classList.add("selected");
+    el.classList.remove("notSelected");
+
+    el.focus();
   });
 });
