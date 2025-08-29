532b5cf664e6252d89c97522fb0db514e76c1d73
Zixus <mahrez.cherif1@gmail.com>
2025-08-29 15:09:55 +0100

    working with the logic and custom menu

--- src/Custom.js
+++ src/Custom.js
@@ -2,7 +2,8 @@ const nodeItems = document.querySelectorAll(".item-card");
 const nodeGender = document.querySelectorAll(".gender-card");
 const nodeSizebtn = document.querySelectorAll(".sizeBtn");
 const nodeColor = document.querySelectorAll(".shirt-color");
-
+const nodePose = document.querySelectorAll(".positon-selection");
+const nodeImgBox = document.querySelectorAll(".custom-img-box");
 //iteme selections
 nodeItems.forEach((el) => {
   el.addEventListener("click", () => {
@@ -60,5 +61,52 @@ nodeColor.forEach((el) => {
     el.classList.remove("notSelected");
 
     el.focus();
+  });
+});
+//Position Selection
+nodePose.forEach((el) => {
+  el.addEventListener("click", () => {
+    if (el.classList.contains("selected")) {
+      el.classList.remove("selected");
+      el.classList.add("notSelected");
+      el.blur();
+      console.log(el);
+    } else {
+      el.classList.add("selected");
+      el.classList.remove("notSelected");
+      console.log(el);
+      el.focus();
+    }
+  });
+});
+//Image Selection
+nodeImgBox.forEach((el) => {
+  el.addEventListener("click", () => {
+    nodeImgBox.forEach((e) => {
+      e.classList.remove("selected");
+      e.classList.add("notSelected");
+    });
+
+    el.classList.add("selected");
+    el.classList.remove("notSelected");
+
+    el.focus();
+  });
+});
+
+// display behaviour
+const customShirt = document.querySelector(".custom-shirt");
+
+nodeItems.forEach((element) => {
+  element.addEventListener("click", () => {
+    if (element.classList.contains("selected")) {
+      console.log(element);
+      if (element.dataset.choice === "shirt") {
+        customShirt.classList.remove("hidden");
+        customShirt.classList.add("show");
+        return;
+      }
+      return;
+    }
   });
 });
